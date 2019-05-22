627bd8aa8325c79579485350c6b56e17a0bb666e
Peter Goes <petergoes@gmail.com>
2019-05-22 13:06:23 +0200

    Chore: format all files

--- test/build-selector-from-ast.test.js
+++ test/build-selector-from-ast.test.js
@@ -1,151 +1,151 @@
 const build = require('../lib/build-selector-from-ast')
 
 test('any', () => {
-    const ast = [
-        {
-            any: [
-                { name: "*", src: "*" }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('*')
+  const ast = [
+    {
+      any: [
+        { name: "*", src: "*" }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('*')
 })
 
 test('Element', async () => {
-    const ast = [
-        {
-            element: [
-                { name: "span", src: "span" }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('span')
+  const ast = [
+    {
+      element: [
+        { name: "span", src: "span" }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('span')
 })
 
 test('Class', async () => {
-    const ast = [
-        {
-            class: [
-                { name: "my-class", src: ".my-class" }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('.my-class')
+  const ast = [
+    {
+      class: [
+        { name: "my-class", src: ".my-class" }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('.my-class')
 })
 
 test('Attribute', async () => {
-    const ast = [
-        {
-            attribute: [
-                { attribute: 'foo="bar"', src: '[foo="bar"]' }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('[foo="bar"]')
+  const ast = [
+    {
+      attribute: [
+        { attribute: 'foo="bar"', src: '[foo="bar"]' }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('[foo="bar"]')
 })
 
 test('Pseudo Class', async () => {
-    const ast = [
-        {
-            pseudoClass: [
-                { name: 'first-child', src: ':first-child' }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe(':first-child')
+  const ast = [
+    {
+      pseudoClass: [
+        { name: 'first-child', src: ':first-child' }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe(':first-child')
 })
 
 test('Pseudo Class with value', async () => {
-    const ast = [
+  const ast = [
+    {
+      pseudoClass: [
         {
-            pseudoClass: [
-                { 
-                    name: 'nth-child',
-                    src: ':nth-child(2)',
-                    children: [
-                        { element: [{ name: '2', src: '2' }] }
-                    ]
-                }
-            ]
+          name: 'nth-child',
+          src: ':nth-child(2)',
+          children: [
+            { element: [{ name: '2', src: '2' }] }
+          ]
         }
-    ]
-    expect(build(ast)).toBe(':nth-child(2)')
+      ]
+    }
+  ]
+  expect(build(ast)).toBe(':nth-child(2)')
 })
 
 test('Pseudo Element', async () => {
-    const ast = [
-        {
-            pseudoElement: [
-                { name: 'after', src: '::after' }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('::after')
+  const ast = [
+    {
+      pseudoElement: [
+        { name: 'after', src: '::after' }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('::after')
 })
 
 test('Pseudo Element with value', async () => {
-    const ast = [
+  const ast = [
+    {
+      pseudoElement: [
         {
-            pseudoElement: [
-                { 
-                    name: 'slotted',
-                    src: '::slotted(h1)',
-                    children: [
-                        { element: [{ name: 'h1', src: 'h1' }] }
-                    ]
-                }
-            ]
+          name: 'slotted',
+          src: '::slotted(h1)',
+          children: [
+            { element: [{ name: 'h1', src: 'h1' }] }
+          ]
         }
-    ]
-    expect(build(ast)).toBe('::slotted(h1)')
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('::slotted(h1)')
 })
 
 test('Pseudo Element with attribute', async () => {
-    const ast = [
+  const ast = [
+    {
+      pseudoElement: [
         {
-            pseudoElement: [
-                { 
-                    name: 'slotted',
-                    src: '::slotted([foo="bar"])',
-                    children: [
-                        { attribute: [{ attribute: 'foo="bar"', src: '[foo="bar"]' }] }
-                    ]
-                }
-            ]
+          name: 'slotted',
+          src: '::slotted([foo="bar"])',
+          children: [
+            { attribute: [{ attribute: 'foo="bar"', src: '[foo="bar"]' }] }
+          ]
         }
-    ]
-    expect(build(ast)).toBe('::slotted([foo="bar"])')
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('::slotted([foo="bar"])')
 })
 
 test('Combinator +', async () => {
-    const ast = [
-        {
-            combinator: [
-                { name: "+", src: "+" }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('+')
+  const ast = [
+    {
+      combinator: [
+        { name: "+", src: "+" }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('+')
 })
 
 test('Combinator ~', async () => {
-    const ast = [
-        {
-            combinator: [
-                { name: "~", src: "~" }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('~')
+  const ast = [
+    {
+      combinator: [
+        { name: "~", src: "~" }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('~')
 })
 
 test('Combinator >', async () => {
-    const ast = [
-        {
-            combinator: [
-                { name: ">", src: ">" }
-            ]
-        }
-    ]
-    expect(build(ast)).toBe('>')
+  const ast = [
+    {
+      combinator: [
+        { name: ">", src: ">" }
+      ]
+    }
+  ]
+  expect(build(ast)).toBe('>')
 })
--- test/transform-css/index.test.js
+++ test/transform-css/index.test.js
@@ -1,42 +1,42 @@
 const transformCss = require('../../lib/transform-css')
 
 test('append element name to selector that does not have :host or :host-context', async () => {
-    expect(await transformCss(
-        'element { background-color: blue; }',
-        'x-element'))
-    .toBe('x-element element { background-color: blue; }')
+  expect(await transformCss(
+    'element { background-color: blue; }',
+    'x-element'))
+  .toBe('x-element element { background-color: blue; }')
 
-    expect(await transformCss(
-        '#id { background-color: blue; }',
-        'x-element'))
-    .toBe('x-element #id { background-color: blue; }')
+  expect(await transformCss(
+    '#id { background-color: blue; }',
+    'x-element'))
+  .toBe('x-element #id { background-color: blue; }')
 
-    expect(await transformCss(
-        '.class { background-color: blue; }',
-        'x-element'))
-    .toBe('x-element .class { background-color: blue; }')
+  expect(await transformCss(
+    '.class { background-color: blue; }',
+    'x-element'))
+  .toBe('x-element .class { background-color: blue; }')
 
-    expect(await transformCss(
-        '[foo="bar"] { background-color: blue; }',
-        'x-element'))
-    .toBe('x-element [foo="bar"] { background-color: blue; }')
+  expect(await transformCss(
+    '[foo="bar"] { background-color: blue; }',
+    'x-element'))
+  .toBe('x-element [foo="bar"] { background-color: blue; }')
 
-    expect(await transformCss(
-        '::slotted(*) { background-color: blue; }',
-        'x-element'))
-    .toBe('x-element *[data-slotted] { background-color: blue; }')
+  expect(await transformCss(
+    '::slotted(*) { background-color: blue; }',
+    'x-element'))
+  .toBe('x-element *[data-slotted] { background-color: blue; }')
 })
 
 test('do not append element name to selector that contains does have :host', async () => {
-    const css = ':host { background-color: blue; }'
-    const elementName = 'x-element'
-    const result = await transformCss(css, elementName)
-    expect(result).toBe('x-element { background-color: blue; }')
+  const css = ':host { background-color: blue; }'
+  const elementName = 'x-element'
+  const result = await transformCss(css, elementName)
+  expect(result).toBe('x-element { background-color: blue; }')
 })
 
 test('do not append element name to selector that contains does have :host-context', async () => {
-    const css = ':host-context(.footer) { background-color: blue; }'
-    const elementName = 'x-element'
-    const result = await transformCss(css, elementName)
-    expect(result).toBe('.footer x-element { background-color: blue; }')
+  const css = ':host-context(.footer) { background-color: blue; }'
+  const elementName = 'x-element'
+  const result = await transformCss(css, elementName)
+  expect(result).toBe('.footer x-element { background-color: blue; }')
 })
